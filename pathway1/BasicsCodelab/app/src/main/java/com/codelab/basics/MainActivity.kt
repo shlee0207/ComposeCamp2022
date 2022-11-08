2a6c50ea6c6f08beb682036460f295cfb6b1de76
Seunghyun <shlee0207@gmail.com>
2022-11-08 23:17:12 +0900

    Jetpack Compose Basic - 7.State hoisting

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -34,47 +38,92 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(
+    modifier: Modifier = Modifier
+) {
+    var shouldShownOnboarding by remember { mutableStateOf(true) }
+    if (shouldShownOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShownOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
     Column(
-        modifier = Modifier.padding(vertical = 4.dp)
+        modifier = modifier.padding(vertical = 4.dp)
     ) {
         for (name in names) {
-            Greeting(name)
+            val expanded = remember { mutableStateOf(false) }
+            val extraPadding = if (expanded.value) 48.dp else 0.dp
+            Surface(
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                Row(modifier = Modifier.padding(24.dp)) {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = extraPadding)
+                    ) {
+                        Text(text = "Hello")
+                        Text(text = "${name}!")
+                    }
+                    Button(
+                        onClick = { expanded.value = !expanded.value }
+                    ) {
+                        Text(if (expanded.value) "Show less" else "Show more")
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(horizontal = 8.dp, vertical = 4.dp)
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello")
-                Text(text = "$name!")
-            }
-            Button(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+        Text("Welcome to the basic Codelab!!")
+        Button(
+            modifier = Modifier.padding(24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun DefaultPreview() {
+private fun GreetingsPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(modifier = Modifier.fillMaxSize())
     }
 }
